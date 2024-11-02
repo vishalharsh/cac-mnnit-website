4f42cddaebc82eab8846e264d3594e1387eb2e59
Vishal Mishra <128250001+vishalharsh@users.noreply.github.com>
2024-11-02 22:57:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// This file can be used for interactivity, like adding event listeners
+// or handling form submissions if you add those features later.
+
+console.log("Welcome to Consulting & Analytics Club MNNIT website!");
